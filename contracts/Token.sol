f29bc47bb1cc29a5e6306ea9782b3bac292bb9dd
sergio <sergioreganati@gmail.com>
2022-10-25 18:15:49 -0400

    3. testing contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,16 @@ import "hardhat/console.sol";
 
 
 contract Token {
-	string public name = "My Token";
+	string public name;
+	string public symbol;
+	//decimals
+	uint256 public decimals = 18;
+	//total supply
+	uint256 public totalSupply;
+
+constructor(string memory _name, string memory _symbol, uint256 _totalsupply){
+	name = _name;
+	symbol = _symbol;
+	totalSupply = _totalsupply * (10**decimals);
+}
 }
