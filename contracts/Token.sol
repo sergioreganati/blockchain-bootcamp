35746885ee1957c183ff1cb79acafb93d3a11472
sergio <sergioreganati@gmail.com>
2022-10-26 15:09:30 -0400

    5. Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,13 +13,40 @@ contract Token {
 	//track balances - state
 	mapping(address => uint256) public balanceOf; //gets the keypar mapps the address to the balance
 
+	event Transfer(
+		address indexed from, 
+		address indexed to, 
+		uint256 value
+	);
+
 	//send tokens - 
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalsupply){
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalsupply)
+	{
 	name = _name;
 	symbol = _symbol;
 	totalSupply = _totalsupply * (10**decimals);
 	//create the tokens with the ownership of the creator/sender of the token, writtes info on the blockchain
 	balanceOf[msg.sender] = totalSupply; 
 	}
+
+	function transfer(address _to, uint256 _value)
+	public 
+	returns (bool success)
+	{
+		//require that senders token has enough tokens to spend
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
+		//deduct tokens from spender
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		//credit tokens to receiver
+		balanceOf[_to] = balanceOf[_to] + _value;
+		//emitt event
+		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
+
 }
--- test/Token.js
+++ test/Token.js
@@ -5,10 +5,12 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe('Token', () => {
+describe('Token', () => 
+{
 	let token,
 		accounts,
-		deployer
+		deployer,
+		receiver
 //making the token avaiable for the tests
 	beforeEach(async () => {
 	//fetch token from blockchain/ deploy
@@ -16,17 +18,15 @@ describe('Token', () => {
 	token = await Token.deploy('Sergio token', 'SSR', '1000000' )
 	accounts = await ethers.getSigners()
 	deployer = accounts[0]
+	receiver = accounts[1]
 	})
 
-describe('Deployment',() => {
-
+describe('Deployment',() => 
+{
 const name = 'Sergio token'
 const symbol = 'SSR'
 const decimals = '18'
 const totalsupply = tokens(1000000)
-
-
-
 it('has the correct name', async ()=> {expect(await token.name()).to.equal('Sergio token')
 })
 it('has the correct symbol', async ()=> {expect(await token.symbol()).to.equal(symbol)
@@ -35,10 +35,64 @@ it('has the correct decimals', async ()=> {expect(await token.decimals()).to.equ
 })
 it('has the correct total supply', async ()=> {expect(await token.totalSupply()).to.equal(totalsupply)
 })
-//console.log(ethers.utils.getAddress(deployer.address.toString()))
+console.log(totalsupply.toString())
 it('assigns total supply to deployer', async ()=> {expect(await token.balanceOf(deployer.address)).to.equal(totalsupply)
 })
+})
+
+describe('Sending tokens', () => {
+	let amount, transaction, result
+
+	describe('Sucess', () => {
+		beforeEach(async () => {
+		amount = tokens(100)
+		transaction = await token.connect(deployer).transfer(receiver.address,amount)
+		result = await transaction.wait() //waits for the transaction to be included in the block
+	})
+
+	it('transfer token balances', async () => { 
+
+		//log balance before transaction
+		//console.log('deployer balance before transaction', await token.balanceOf(deployer.address))
+		//console.log('receiver balance before transaction', await token.balanceOf(receiver.address))
+
+		//transfer tokens transaction must be signed so connect and ensure balance changes
+		expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+		expect (await token.balanceOf(receiver.address)).to.equal(amount)
+
+		//log balance after transaction
+		//console.log('deployer balance before transaction', await token.balanceOf(deployer.address))
+		//console.log('receiver balance before transaction', await token.balanceOf(receiver.address))
+	})
+
+	it('emits a transfer event', async () => {
+		const eventlog = result.events[0]
+		//console.log(eventlog)
+		expect(eventlog.event).to.equal('Transfer')
+		const args = eventlog.args
+		expect(args.from).to.equal(deployer.address) 
+		//compares line 'from' in the eventlog to deployer address
+		expect(args.to).to.equal(receiver.address)
+		expect(args.value).to.equal(amount)
+	})
+
+	describe('Failure', () => {
+		it('rejects insuficcient balances', async () => {
+		//transfer more tokens the deployer has
+		const invalidamount = tokens(10000000)	
+		await expect(token.connect(deployer).transfer(receiver.address,invalidamount)).to.be.reverted
+		})
+		it('rejects invalid recipent', async () => {
+        const amount = tokens(100)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      	})
+
+	})
 
 })
+	
+	
+})
+
 })
 
