eaddf02b3d9fa33ad348e05cd82445e76653282f
sergio <sergioreganati@gmail.com>
2022-10-26 11:50:27 -0400

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,14 +6,20 @@ import "hardhat/console.sol";
 contract Token {
 	string public name;
 	string public symbol;
-	//decimals
+	//decimals - state
 	uint256 public decimals = 18;
-	//total supply
+	//total supply - state
 	uint256 public totalSupply;
+	//track balances - state
+	mapping(address => uint256) public balanceOf; //gets the keypar mapps the address to the balance
 
-constructor(string memory _name, string memory _symbol, uint256 _totalsupply){
+	//send tokens - 
+
+	constructor(string memory _name, string memory _symbol, uint256 _totalsupply){
 	name = _name;
 	symbol = _symbol;
 	totalSupply = _totalsupply * (10**decimals);
-}
+	//create the tokens with the ownership of the creator/sender of the token, writtes info on the blockchain
+	balanceOf[msg.sender] = totalSupply; 
+	}
 }
--- test/Token.js
+++ test/Token.js
@@ -6,12 +6,16 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let token
+	let token,
+		accounts,
+		deployer
 //making the token avaiable for the tests
 	beforeEach(async () => {
 	//fetch token from blockchain/ deploy
 	const Token = await ethers.getContractFactory('Token')
 	token = await Token.deploy('Sergio token', 'SSR', '1000000' )
+	accounts = await ethers.getSigners()
+	deployer = accounts[0]
 	})
 
 describe('Deployment',() => {
@@ -25,13 +29,14 @@ const totalsupply = tokens(1000000)
 
 it('has the correct name', async ()=> {expect(await token.name()).to.equal('Sergio token')
 })
-
 it('has the correct symbol', async ()=> {expect(await token.symbol()).to.equal(symbol)
 })
 it('has the correct decimals', async ()=> {expect(await token.decimals()).to.equal(decimals)
 })
-
 it('has the correct total supply', async ()=> {expect(await token.totalSupply()).to.equal(totalsupply)
+})
+//console.log(ethers.utils.getAddress(deployer.address.toString()))
+it('assigns total supply to deployer', async ()=> {expect(await token.balanceOf(deployer.address)).to.equal(totalsupply)
 })
 
 })
